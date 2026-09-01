87f2127e4c769cbaf4c163e1e06a5ae3bc842e81
mohamadrestu7 <mohamadrestu@gmail.com>
2026-09-01 23:42:58 +0700

    first commit

---
+++ src/test/java/com/restu/fintech/FintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restu.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
