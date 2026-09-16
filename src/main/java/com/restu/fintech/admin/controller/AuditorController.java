b06414cc2148890da192bb9d24533fbba219419c
mohamadrestu7 <mohamadrestu@gmail.com>
2026-09-17 00:30:34 +0700

    verivy account number now accesseble to customer

--- src/main/java/com/restu/fintech/admin/controller/AuditorController.java
+++ src/main/java/com/restu/fintech/admin/controller/AuditorController.java
@@ -20,16 +20,17 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/audit")
-@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('AUDITOR')")
 public class AuditorController {
 
     private final AuditorService auditorService;
 
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('AUDITOR')")
     @GetMapping("/totals")
     public ResponseEntity<Map<String, Long>> getSystemTotals() {
         return ResponseEntity.ok(auditorService.getSystemsTotals());
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('AUDITOR')")
     @GetMapping("/users")
     public ResponseEntity<UserDTO> findUserByEmail(@RequestParam String email) {
 
@@ -48,6 +49,7 @@ public class AuditorController {
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('AUDITOR')")
     @GetMapping("/transactions/by-account")
     public ResponseEntity<List<TransactionDTO>> findTransactionsByAccountNumber(@RequestParam String accountNumber) {
 
@@ -60,6 +62,7 @@ public class AuditorController {
         return ResponseEntity.ok(transactionDTOS);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('AUDITOR')")
     @GetMapping("/transaction/by-id")
     public ResponseEntity<TransactionDTO> findTransactionById(@RequestParam Long id) {
 
